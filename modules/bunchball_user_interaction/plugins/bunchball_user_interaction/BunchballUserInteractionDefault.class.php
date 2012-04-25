ac42926090d669810e3b15335091867a398e4627
andremolnar <andre@becircle.com>
2012-04-25 13:57:56 -0400

    BUB-2 Working storage of settings now after a long and tiring search for how to do method calls as submit callbacks in FAPI--- you can't

--- modules/bunchball_user_interaction/plugins/bunchball_user_interaction/BunchballUserInteractionDefault.class.php
+++ modules/bunchball_user_interaction/plugins/bunchball_user_interaction/BunchballUserInteractionDefault.class.php
@@ -5,9 +5,11 @@ class BunchballUserInteractionDefault {
   //unlikely that I need to have the api object in here, but maybe
   //public $api;
   //
-  //function __construct(NitroAPI $api) {
-  //  $this->api = $api;
-  //}
+  function __construct() {
+
+    $this->options['bunchball_user_login'] = variable_get('bunchball_user_login','');
+    $this->options['bunchball_user_register'] = variable_get('bunchball_user_register', '');
+  }
 
   public function adminForm($form, &$form_state) {
     $form['bunchball_user_login'] = array(
@@ -15,7 +17,7 @@ class BunchballUserInteractionDefault {
       '#title' => t('Communicate user login'),
       '#default_value' => $this->options['bunchball_user_login'],
     );
-    
+
     $form['bunchball_user_register'] = array(
       '#type' => 'checkbox',
       '#title' => t('Communicate new user registration'),
@@ -23,5 +25,14 @@ class BunchballUserInteractionDefault {
     );
 
     return $form;
+  }
+
+  public function adminFormValidate($form, &$form_state) {
+
+  }
+
+  public function adminFormSubmit($form, &$form_state) {
+    isset($form_state['values']['bunchball_user_login']) ? variable_set('bunchball_user_login', $form_state['values']['bunchball_user_login']) : '';
+    isset($form_state['values']['bunchball_user_register']) ? variable_set('bunchball_user_register', $form_state['values']['bunchball_user_register']) : '';
   }
 }
