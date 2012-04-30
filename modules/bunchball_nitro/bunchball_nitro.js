4cc0ceb2ca79f5898f5e12a62eac6ff3860186f1
Jason <jswaby@Jasons-MacBook-Pro-3.local>
2012-04-30 11:00:40 -0400

    filling out the JavaScript File adding more comments

--- modules/bunchball_nitro/bunchball_nitro.js
+++ modules/bunchball_nitro/bunchball_nitro.js
@@ -1,42 +1,39 @@
 // JavaScript closure. Defined jQuery $.
 (function ($) {
   $(document).ready(function() {
-      // bind the actions to functions below
+      // TODO: bind the actions to functions below
   });
   
 }) (jQuery);
 
 
-function doSomethingFun() {
-    alert('This Is Fun!!');
-}
-
 function userViewedPhoto() {
-  submitNitroAPICall(tags);
+  submitNitroAPICall('PHOTO_VIEWED');
 }
 
 function userViewedVideo() {
-  submitNitroAPICall(tags);
+  submitNitroAPICall('PHOTO_VIEWED');
 }
 
-function userViewedPhoto() {
-  submitNitroAPICall(tags);
+function userViewedNode() {
+  submitNitroAPICall('CONTENT_VIEWED');
 }
 
 function submitNitroAPICall(tags) {
   if(nitro == null) {
     return;
   }
-  
+
+  // TODO: populate this properly with session Key in Drupal.settings..
   var sessionKey = 'thisIsASessionKeyFromUser.Login';
   
   var queryString = 'method=user.logAction&sessionKey=' + sessionKey + '&tags=';
   queryString += tags;
   
   alert(queryString + ' was called');
-  // nitro.callAPI(queryString, "nitroCallback"); 
+  // TODO: nitro.callAPI(queryString, "nitroCallback"); 
 }
 
 function nitroCallback(data, token) {
-  
+  // this is a stub that can be used later to track responses from the server.
 }
